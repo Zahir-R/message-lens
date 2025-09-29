3914a7f6f88945429997d99d7cc119eab2be5aa5
Zahir-R <zahiriv4@gmail.com>
2025-09-28 22:06:44 -0400

    Using ethereal mail

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -55,11 +55,12 @@ export async function POST(request: NextRequest) {
         }
 
         const transporter = nodemailer.createTransport({
-            service: 'outlook',
+            host: 'smtp.ethereal.email',
+            port: 587,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
-            },
+            }
         });
 
         const mailOptions = {
